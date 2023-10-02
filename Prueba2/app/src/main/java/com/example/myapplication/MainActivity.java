c27adb862372d120bf037248775c99e7e2ecea21
Santi <105241651+smonmir@users.noreply.github.com>
2023-10-02 12:46:46 +0200

    HolaMundo

---
+++ Prueba2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnSaludar = (Button) findViewById(R.id.btnSaludar);
+
+        btnSaludar.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        TextView textMensaje = findViewById(R.id.txtSaludo);
+        textMensaje.setText("Funciona");
+    }
+
+
+    /*
+    public void cambiaMensaje(View view){
+        TextView textMensaje = findViewById(R.id.txtSaludo);
+        textMensaje.setText("Funciona");
+    }
+    */
+
+
+}
