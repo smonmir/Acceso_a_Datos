efe4897be1f3b31501c0b9894177570cc18962db
Santi <105241651+smonmir@users.noreply.github.com>
2023-10-16 12:45:26 +0200

    Pizzeria

---
+++ Android_Studio/Pizzeria/app/src/main/java/com/example/pizzeria/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.pizzeria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnElegirPizza;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnElegirPizza = findViewById(R.id.btnElegirPizza);
+
+        btnElegirPizza.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btnElegirPizza){
+
+        }
+    }
+}
