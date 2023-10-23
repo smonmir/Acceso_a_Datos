e7af4201f9295d4646ff7a4ace1641522938a60f
Santi <105241651+smonmir@users.noreply.github.com>
2023-10-23 09:23:55 +0200

    Pizzeria

--- Android_Studio/Pizzeria/app/src/main/java/com/example/pizzeria/Loggin.java
+++ Android_Studio/Pizzeria/app/src/main/java/com/example/pizzeria/Loggin.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.TextView;
 
 import com.example.pizzeria.Servicio.Servicio;
+
+import java.nio.file.attribute.FileTime;
 
 public class Loggin extends AppCompatActivity {
 
@@ -46,10 +52,15 @@ public class Loggin extends AppCompatActivity {
                     startActivity(i);
                 }
                 else{
+
                     AlertDialog.Builder builder = new AlertDialog.Builder(Loggin.this);
                     builder.setMessage("Usuario o contraseña incorrecto.");
 
                     AlertDialog dialog = builder.create();
+
+
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.GRAY));
+
                     dialog.show();
                     new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
                         @Override
