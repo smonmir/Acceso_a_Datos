14f2c692b0a59a65cd282d617ee8a3f80b120844
smonmir <105241651+smonmir@users.noreply.github.com>
2023-12-15 14:18:10 +0100

    a

---
+++ NetBeans/Mockito/src/main/java/com/mycompany/mockito/Servicio.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mockito;
+
+/**
+ *
+ * @author santi
+ */
+public class Servicio {
+        
+    public int sumar(int a, int b){
+        return a+b;
+    }
+    
+    public int restar(int a, int b){
+        return a-b;
+    }
+}
